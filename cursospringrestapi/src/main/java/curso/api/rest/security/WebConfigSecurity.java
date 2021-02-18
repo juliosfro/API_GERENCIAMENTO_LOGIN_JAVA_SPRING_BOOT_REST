d707739a7b047b07e5d178fad59a8cec8e2f63e6
JULIO DE OLIVEIRA NETO <juliosfro@gmail.com>
2021-02-18 13:29:42 -0300

    Implementacao das config. de segurança.

---
+++ cursospringrestapi/src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -0,0 +1,57 @@
+package curso.api.rest.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import curso.api.rest.service.ImplementationUserDetailsService;
+
+/* Mapeia URL, endereços, autoriza ou bloqueia acessos a URLS. */
+/* Temos que extender da classe WebSecurityConfigurerAdapter */
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+	/* Configurações de solicitação de acesso HTTP */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/* Ativando a proteção contra usuarios que não estão validados por token. */
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+
+				/* Ativando restrição a URL. */
+				/* Abaixo esta dizendo que a parte inicial do sistema será publica. */
+				.disable().authorizeRequests().antMatchers("/").permitAll().antMatchers("/index").permitAll()
+
+				/* URL de logout que redireciona após o user deslogar do sistema. */
+				.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+
+				/* Mapeia URL de logout e invalida o usuario */
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+
+		/* Filtra requisições de login para autenticação */
+
+		/*
+		 * Filtra demais requisições para verificar a presença do token JWT no HEADER
+		 * HTTP.
+		 */
+	}
+
+	/* Abaixo esta a implementacao que fizemos para consultar no banco de dados. */
+	@Autowired
+	private ImplementationUserDetailsService implementationUserDetailsService;
+
+	/* Vamos customizar essa autenticação. */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		/* Service que irá consultar o usuario no banco de dados. */
+		/* Usamos um padrão de codificação de senha. */
+		auth.userDetailsService(implementationUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+}
---
+++ cursospringrestapi/src/main/java/curso/api/rest/service/ImplementationUserDetailsService.java
@@ -0,0 +1,33 @@
+package curso.api.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+
+@Service
+public class ImplementationUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		/* Consultar no banco de dados o usuario. */
+		Usuario usuario = usuarioRepository.findUserByLogin(username);
+
+		if (usuario == null) {
+			throw new UsernameNotFoundException("Usuário não encontrado.");
+		}
+
+		/* Vamos construir um usuario proprio do Spring para autenticação. */
+		return new User(usuario.getLogin(), usuario.getPassword(), usuario.getAuthorities());
+	}
+
+}
