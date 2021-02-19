4b9b5a6505ac5a5e33fa16e137768a9555115699
JULIO DE OLIVEIRA NETO <juliosfro@gmail.com>
2021-02-19 09:01:00 -0300

    Implementação do gerenciador de token JWT

--- cursospringrestapi/src/main/java/curso/api/rest/security/WebConfigSecurity.java
+++ cursospringrestapi/src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import curso.api.rest.service.ImplementationUserDetailsService;
@@ -32,14 +33,17 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
 
 				/* Mapeia URL de logout e invalida o usuario */
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 
-		/* Filtra requisições de login para autenticação */
+				/* Filtra requisições de login para autenticação */
+				.and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+						UsernamePasswordAuthenticationFilter.class)
 
-		/*
-		 * Filtra demais requisições para verificar a presença do token JWT no HEADER
-		 * HTTP.
-		 */
+				/*
+				 * Filtra demais requisições para verificar a presença do token JWT no HEADER
+				 * HTTP
+				 */
+				.addFilterBefore(new JwtApiAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	/* Abaixo esta a implementacao que fizemos para consultar no banco de dados. */
