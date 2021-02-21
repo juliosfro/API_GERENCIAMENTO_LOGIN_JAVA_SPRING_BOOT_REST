fdc26e1db9a57f5c015708f154993b565f0a39fc
JULIO DE OLIVEIRA NETO <juliosfro@gmail.com>
2021-02-20 21:47:13 -0300

    Criação de Classes de Tratamento de Excessoes

---
+++ cursospringrestapi/src/main/java/curso/api/rest/ObjectError.java
@@ -0,0 +1,24 @@
+package curso.api.rest;
+
+public class ObjectError {
+
+    private String error;
+    private String codeError;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getCodeError() {
+        return codeError;
+    }
+
+    public void setCodeError(String codeError) {
+        this.codeError = codeError;
+    }
+
+}
